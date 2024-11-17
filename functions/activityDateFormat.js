2f2e2ee5eae7862f0071a8ecce6903313814cf2c
creynolds8 <cameron8.cr@gmail.com>
2024-11-17 15:37:37 -0800

    fix error in picking month and weekday names

--- functions/activityDateFormat.js
+++ functions/activityDateFormat.js
@@ -3,9 +3,9 @@ const dateFormat = (timestamp) => {
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
   const date = new Date(timestamp);
-  const weekday = days[(date.getDay() - 1)];
+  const weekday = days[date.getDay()];
   const day = date.getDate();
-  const month = months[(date.getMonth() - 1)];
+  const month = months[date.getMonth()];
   const year = date.getFullYear();
   dateString = `${weekday}. ${month}. ${day}, ${year}`;
   return dateString;
