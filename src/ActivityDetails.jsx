96f7cec73221398c42ec6ec6d045016d1f1da7d0
creynolds8 <cameron8.cr@gmail.com>
2024-11-17 15:37:53 -0800

    setup activity details

---
+++ src/ActivityDetails.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import "./css/ActivityDetails.css"
+import activityTime from "../functions/activityTimeFormat";
+import dateFormat from "../functions/activityDateFormat";
+
+const ActivityDetails = ({ activity }) => {
+  return (
+    <div className="row activity-details">
+      <div className="col">
+        <span>
+
+        </span>
+        <span>
+          {dateFormat(activity.created_at)}
+        </span>
+        <span>
+          {activityTime(activity.created_at)}
+        </span>
+      </div>
+      <div className="col">
+        <span>
+          Call made via Aircall: {activity.via}
+        </span>
+        <span>
+          Call duration: {activity.duration}
+        </span>
+        <span>
+          Status: {activity.call_type}
+        </span>
+      </div>
+    </div>
+
+  );
+};
+
+export default ActivityDetails;
